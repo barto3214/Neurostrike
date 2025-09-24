9b1bfdf64d06b37c399b4b33f093d4a26ca70735
barto3214 <bartek.slusarz@onet.pl>
2025-09-24 11:34:55 +0200

    Added resolution changing

--- Assets/Scripts/Game_manager.cs
+++ Assets/Scripts/Game_manager.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game_manager : MonoBehaviour
 {
@@ -9,6 +11,22 @@ public class Game_manager : MonoBehaviour
     [SerializeField] GameObject options_container;
     public int enemies_to_kill = 0;
 
+    Dictionary<string, (int, int, bool)> resolutions = new Dictionary<string, (int, int, bool)> {
+        {"1920", (1920, 1080, true)},
+        {"1280", (1280, 720, true)},
+        {"800", (800, 600, false)}
+    };
+
+    string[] resolutionKeys = new string[] { "1920", "1280", "800" };
+
+    [SerializeField] TMPro.TMP_Dropdown resolutionDropdown;
+
+
+    void Start()
+    {
+        // Dodajemy listener do Dropdown
+        resolutionDropdown.onValueChanged.AddListener(OnResolutionDropdownChanged);
+    }
     public void adjustEnemiesLeft(int enemies_left)
     {
         enemies_to_kill += enemies_left;
@@ -51,5 +69,11 @@ public class Game_manager : MonoBehaviour
     public void Options()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Options");
+    }
+
+    public void OnResolutionDropdownChanged(int index)
+    {
+        Screen.SetResolution(resolutions[resolutionKeys[index]].Item1, resolutions[resolutionKeys[index]].Item2, resolutions[resolutionKeys[index]].Item3);
+        Debug.Log("Resolution changed to: " + resolutions[resolutionKeys[index]].Item1 + "x" + resolutions[resolutionKeys[index]].Item2 + " Fullscreen: " + resolutions[resolutionKeys[index]].Item3);
     }
 }
